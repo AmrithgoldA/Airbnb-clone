6f5badd46f1f641d7ea552f715a4b883000eb5a4
amrithgold <goldandgold1002@gmail.com>
2024-05-22 09:45:11 +0530

    initial commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    
+    return (
+        <section className="hero-component">
+            <img className = "hero-image" src="/images/photo-grid.png" />
+            <h1 className="hero-heading">Online Experiences</h1>
+            <p className="hero-paragraph">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+
+};
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(): any {
+
+    return (
+        <div className="navbar-container">
+            <img className = "bnblogo" src="/images/airbnb-logo.png" />
+        </div>
+    )
+
+};
